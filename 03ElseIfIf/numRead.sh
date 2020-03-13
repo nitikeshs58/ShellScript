60e4d903680f723094f601f0f7b58f039bd6ea42
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-13 17:35:51 +0530

    Added: Number in word like 1 one 10 ten.

---
+++ 03ElseIfIf/numRead.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+read -p "Enter a number : " num
+
+if [[ $num -eq 1 ]]
+then
+	echo "One"
+
+elif [[ $num -eq 10 ]]
+then
+	echo "Ten"
+
+elif [[ $num -eq 100 ]]
+then
+	echo "Hundred"
+
+elif [[ $num -eq 1000 ]]
+then
+	echo "One Thousand "
+
+else
+	echo "!!! Envalid : Enter only1 10 100  like this!!!"
+fi
+
+
