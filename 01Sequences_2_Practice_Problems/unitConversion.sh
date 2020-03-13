913316323fc58c0dc4722a95ca08ad3cdbb36eb2
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-13 11:01:19 +0530

    Modified : added areaOfPlots and area in Acres conversion.

--- 01Sequences_2_Practice_Problems/unitConversion.sh
+++ 01Sequences_2_Practice_Problems/unitConversion.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -x
 
-read -p "Enter num in inch" inch
-feet=`expr "scale=2;$inch/12" | bc`
+read	-p	"Enter num in inch"	inch
+feet=`expr	"scale=2;$inch/12"	|	bc`
+
+read	-p	"Enter lenth in feet :"		l
+read	-p	"Enter breadth in feet:"	b
+areaOfPlot=`expr	"scale=2;$l*$b/3.28"	|	bc`
+
+
+plotAcres=`expr	"scale=2;$areaOfPlot*25/4048.8"	|	bc`
+
+
+
+
+
