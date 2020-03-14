a17cd99f88938f78eca1be688a152090a30ef032
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-14 11:07:51 +0530

    Modified: Fixed indentation and commemts.

--- 05ForLoop/powerOfTwo.sh
+++ 05ForLoop/powerOfTwo.sh
@@ -5,6 +5,5 @@ read -p "Enter power of 2 : " power
 
 for (( counter=1; counter<=$power; counter++ ))
 do
-			echo "$(($number**$counter))"  # "**" means power of 
+	echo "$(($number**$counter))" 
 done
-
