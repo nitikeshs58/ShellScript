10239a11696b4a6e40ef00c1710a81f9c6ae47b8
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-14 11:05:11 +0530

    Modified: Fixed indentation.

--- 05ForLoop/primeNumber.sh
+++ 05ForLoop/primeNumber.sh
@@ -4,16 +4,16 @@ read -p "Enter a number: " num
 count=0
 for (( counter=1; counter<=$num; counter++ ))
 do
-		if [[ $num%$counter -eq 0 ]]
-		then
-				count=$count+1
-		fi
+	if [[ $num%$counter -eq 0 ]]
+	then
+		count=$count+1
+	fi
 done
 
 if [[ $count -eq 2 ]]
 then
-		echo "Prime number "
+	echo "Prime number "
 else
-		echo "Non Prime Number "
+	echo "Non Prime Number "
 fi
 
