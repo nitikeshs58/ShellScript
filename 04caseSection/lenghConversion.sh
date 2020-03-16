dc061b96235c92e7895feef3b4093efad64e6f35
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-16 18:28:48 +0530

    Modified: Fixed Indentation.

--- 04caseSection/lenghConversion.sh
+++ 04caseSection/lenghConversion.sh
@@ -5,22 +5,22 @@ read -p "Enter your choice in between 1 to 4: " choice
 
 case $choice in
 		1)
-			echo "Feet to Inch"
-			feetToInch=`expr "scale=2;$len*12" | bc`
+		echo "Feet to Inch"
+		feetToInch=`expr "scale=2;$len*12" | bc`
 		;;
 		2)
-			echo "Feet to Meter"
-			feetToMeter=`expr "scale=2;$len*0.3048" | bc`
+		echo "Feet to Meter"
+		feetToMeter=`expr "scale=2;$len*0.3048" | bc`
 		;;
 		3)
-			echo "Inch to Feet"
-			inchToFeet=`expr "scale=2;$len/12" | bc`
+		echo "Inch to Feet"
+		inchToFeet=`expr "scale=2;$len/12" | bc`
 		;;
 		4)
-			echo "Meter to Feet"
-			meterToFeet=`expr " $len/0.3048" | bc`
+		echo "Meter to Feet"
+		meterToFeet=`expr " $len/0.3048" | bc`
 		;;
 		*)
-			echo "!!Choice invalid!!"
+		echo "!!Choice invalid!!"
 		;;
 esac
