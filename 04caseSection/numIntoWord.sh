0be83fda19d4ec70657a428fa42239cac7a928cc
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-16 18:43:35 +0530

    Modified: Fixed Indentation.

--- 04caseSection/numIntoWord.sh
+++ 04caseSection/numIntoWord.sh
@@ -4,35 +4,35 @@ read -p "Enter a single digit number : " num
 
 case $num in
 		1)
-			echo "One"
-			;;
-      2)
-         echo "Two"
-         ;;
-      3)
-         echo "Three"
-         ;;
-      4)
-         echo "Four"
-         ;;
-      5)
-         echo "Five"
-         ;;
-      6)
-         echo "Six"
-         ;;
-      7)
-         echo "Seven"
-         ;;
-      8)
-         echo "Eight"
-         ;;
-      9)
-         echo "Nine"
-         ;;
-      0)
-         echo "Zero"
-         ;;
+		echo "One"
+		;;
+		2)
+		echo "Two"
+		;;
+		3)
+		echo "Three"
+		;;
+		4)
+		echo "Four"
+		;;
+		5)
+		echo "Five"
+		;;
+		6)
+		echo "Six"
+		;;
+		7)
+		echo "Seven"
+		;;
+		8)
+		echo "Eight"
+		;;
+		9)
+		echo "Nine"
+		;;
+		0)
+		echo "Zero"
+		;;
 		*)
-			echo "!!!Enter only single digit!!!"
+		echo "!!!Enter only single digit!!!"
 esac
