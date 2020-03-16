a225311b7b34886cbadbae9f70b6dc01b31e1213
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-16 18:47:26 +0530

    Modified: Fixed Indentation.

--- 05ForLoop/harmonicNumber.sh
+++ 05ForLoop/harmonicNumber.sh
@@ -5,6 +5,6 @@ read -p "Enter nth number : " num
 
 for (( counter=1; counter<=$num; counter++ ))
 do
-		harmonicNumber=`expr "scale=4;$harmonicNumber+1/$counter" | bc`
+	harmonicNumber=`expr "scale=4;$harmonicNumber+1/$counter" | bc`
 done
 	echo $harmonicNumber
